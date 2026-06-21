b3e4115c810de87f1b2a4508f083c1dd4b60d3d3
Junseo Bae <ferrater1013@gmail.com>
2026-06-21 18:06:31 +0900

    chore: asm-practice 개요 및 베이스라인 구축

---
+++ src/main/kotlin/io/bebeis/asmpractice/AsmPracticeApplication.kt
@@ -0,0 +1,11 @@
+package io.bebeis.asmpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AsmPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<AsmPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/io/bebeis/asmpractice/AsmPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.bebeis.asmpractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AsmPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
